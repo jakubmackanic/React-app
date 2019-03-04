140754ea973cfef8bda2a2d1d9a8dfbe41cf0cf4
Jakub Mackanič <33807475+narayana108@users.noreply.github.com>
2019-03-04 21:46:11 +0100

    Updates

--- precache-manifest.e815e7ddd6e1e5e4e020bdf804e59be3.js
+++ precache-manifest.e815e7ddd6e1e5e4e020bdf804e59be3.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/React-app/static/js/runtime~main.9989a70d.js"
   },
   {
-    "revision": "222757bb701794fcbb94",
-    "url": "/React-app/static/js/main.222757bb.chunk.js"
+    "revision": "7a19c541c409cfc800d6",
+    "url": "/React-app/static/js/main.7a19c541.chunk.js"
   },
   {
-    "revision": "54003f390578fe00e871",
-    "url": "/React-app/static/js/2.54003f39.chunk.js"
+    "revision": "df25eaf5d71178a739bb",
+    "url": "/React-app/static/js/2.df25eaf5.chunk.js"
   },
   {
-    "revision": "222757bb701794fcbb94",
+    "revision": "7a19c541c409cfc800d6",
     "url": "/React-app/static/css/main.2fcda82c.chunk.css"
   },
   {
-    "revision": "54003f390578fe00e871",
+    "revision": "df25eaf5d71178a739bb",
     "url": "/React-app/static/css/2.f7d56095.chunk.css"
   },
   {
-    "revision": "ee7663f45c265d2ce0698fdcb12d6678",
+    "revision": "9ba7cd1b7214fa8a137fb9e2892e8a39",
     "url": "/React-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-app/precache-manifest.2b39f1da36ab30bfd010720cf4502f4a.js"
+  "/React-app/precache-manifest.e815e7ddd6e1e5e4e020bdf804e59be3.js"
 );
 
 workbox.clientsClaim();
